3652519bfc4384871fa143c07dd7b6962cb9e868
ohhsj <jacksonoh@gmail.com>
2017-02-14 17:36:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def makeWebhookResult(req):
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech = "This is a test response back to the webhook"
 
     print("Response:")
     print(speech)
