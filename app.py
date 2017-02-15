e03abadef6ff7b4536cdce23d0ac7ece44b6f658
ohhsj <jacksonoh@gmail.com>
2017-02-15 12:57:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,27 +39,36 @@ def makeWebhookResult(req):
     stddealsize = int(dealsize) / 100000.0
     
     #Assumes the following for PV estimations
-    #                           Trades	Scenarios	Horizons	Calcs
-    #Market Risk Sensitivities	100000	20	        1	        1
-    #HSVaR	                    100000	500	        1	        1
-    #Monte Carlo VaR	        100000	5000	    1	        1
-    #MC VaR Factor Groups	    100000	5000	    1	        10
-    #Additional VaR Horizons	100000	5000	    5	        10
-    #Potential Future Exposure	100000	5000	    100	        1
-    #PFE Stress Tests	        100000	5000	    100	        10
-    #CVA Sensitivities	        100000	5000	    100	        50
+    #                           Trades	Scenarios	Horizons	Calcs   Total
+    #Market Risk Sensitivities	100000	20	        1	        1       2000000
+    #HSVaR	                    100000	500	        1	        1       50000000
+    #Monte Carlo VaR	        100000	5000	    1	        1       500000000
+    #MC VaR Factor Groups	    100000	5000	    1	        10      5000000000
+    #Additional VaR Horizons	100000	5000	    5	        10      25000000000
+    #Potential Future Exposure	100000	5000	    100	        1       50000000000
+    #PFE Stress Tests	        100000	5000	    100	        10      500000000000
+    #CVA Sensitivities	        100000	5000	    100	        50      2500000000000
 
     #in millions
-    stdPVLookup = {'FRTB-SA': 10, 'HS VaR': 50, 'Monte Carlo VaR': 500, 'FRTB HS-IMA': 1000, 'PFE': 5000, 'PFE Stress Tests': 500000, 'FRTB-CVA': 2500000}
+    stdPVLookup = {
+        'Market Risk Sensitivities': 2,
+        'FRTB-SA': 10, 
+        'HS VaR': 50, 
+        'Monte Carlo VaR': 500, 
+        'FRTB HS-IMA': 1000, 
+        'PFE': 5000, 
+        'PFE Stress Tests': 500000, 
+        'FRTB-CVA': 2500000
+    }
     
     dealPV = stddealsize * stdPVLookup[calculationtype]
     
-    #Naked cores is 30 million PVs for 2x8 core and 10 adhoc runs i.e each run to complete in 2 hours
+    #Naked cores is 60 million PVs for 2x8 core and 10 adhoc runs i.e each run to complete in 2 hours
     #Looking at around 250 valuations per core per second
-    numofCores = math.ceil(dealPV / 30) * 8
+    numofCores = math.ceil(dealPV / 60) * 8
     
     #RAM size
-    RAM = math.ceil(dealPV / 30) * 32
+    RAM = math.ceil(dealPV / 60) * 32
     
     speech = "Estimated PV Calculations (in million): %s, Num of Cores: 2 x %s, RAM: %s, Calculation Type: %s." % (str(dealPV),str(numofCores),str(RAM),calculationtype)
 
